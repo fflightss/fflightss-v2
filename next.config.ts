3d5f6da937149239ea8c6c95481d39b5d88bdd45
fflightss <pcogouk@gmail.com>
2026-06-17 09:56:46 +0100

    Fix Images

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
       },
     ],
   },
